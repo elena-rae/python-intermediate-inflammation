04bc47a5cdf20641850f30658e7c026164ce0de9
elena-rae <elena.raetsch@posteo.de>
2024-07-04 11:54:05 +0200

    changes in test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -24,7 +24,7 @@ def test_daily_mean(test, expected):
         ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [4, -1, 9]),
     ])
 def test_daily_max(test, expected):
-    """Test mean function works for array of zeroes and positive integers."""
+    """Test max function works for array of zeroes and integers."""
     from inflammation.models import daily_max
     npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
@@ -36,8 +36,8 @@ def test_daily_max(test, expected):
         ([ [1, 2], [3, 4], [5, 6] ], [1, 2]),
         ([ [4, -2, 5], [1, -6, 2], [-4, -1, 9] ], [-4, -6, 2]),
     ])
-def test_daily_max(test, expected):
-    """Test mean function works for array of zeroes and positive integers."""
+def test_daily_min(test, expected):
+    """Test min function works for array of zeroes and integers."""
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
