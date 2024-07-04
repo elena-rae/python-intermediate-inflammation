0ba75d5f146c2c77bc164b8842053672e339ee73
elena-rae <elena.raetsch@posteo.de>
2024-07-04 13:21:28 +0200

    Add defensive programming - rais value error in patient_normalise

--- inflammation/models.py
+++ inflammation/models.py
@@ -46,3 +46,28 @@ def daily_min(data):
     :returns: An array of minimum values of measurements for each day.
     """
     return np.min(data, axis=0)
+
+
+def patient_normalise(data):
+    """Normalise patient data from a 2D inflammation data array.
+    
+    NaN values are ignored, and normalised to 0.
+    Negative values are rounded to 0.
+
+    :param data: A 2D data array with inflammation data (each row contains measurements for
+    a single patient across all days).
+    :returns: An array with the normalized measurements.
+    """
+
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+
+    if len(data.shape) != 2:
+        raise ValueError("Data should be two dimensional")
+
+    maximum = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / maximum[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
--- tests/test_models.py
+++ tests/test_models.py
@@ -41,10 +41,57 @@ def test_daily_min(test, expected):
     from inflammation.models import daily_min
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-
 def test_daily_min_string():
     """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
 
     with pytest.raises(TypeError):
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
+
+@pytest.mark.parametrize(
+        "test, expected, expect_raises",
+        [
+            (
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                None,
+            ),
+            (
+                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+                [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+                None
+            ),
+            (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+            ),
+            (
+                [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+                None,
+            ),
+            (
+                [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                ValueError,
+            ),
+            (
+                [[[-1, 2, 3], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]],
+                [[[0, 0.67, 1], [0.67, 0.83, 1]], [[0.78, 0.89, 1], [0.78, 0.89, 1]]],
+                ValueError,
+            ),
+        ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers.
+       Test with a relative and absolute tolerance of 0.01."""
+    from inflammation.models import patient_normalise
+    
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+
