6feca3eaf4b4b777ab1194f68772b11cda5ef979
elena-rae <elena.raetsch@posteo.de>
2024-07-05 11:45:59 +0200

    move data loading to separate function

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -7,6 +7,17 @@ import numpy as np
 
 from inflammation import models, views
 
+def load_data(data_path): 
+    """load all csv files from directory 
+
+    :param data_path: dame of directory containing csv file 
+    : type data_path: str
+    """
+
+    data_file_paths = glob.glob(os.path.join(data_path, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_path}")
+    return list(map(models.load_csv, data_file_paths))
 
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
@@ -14,11 +25,8 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
+    
+    data = load_data(data_dir)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
